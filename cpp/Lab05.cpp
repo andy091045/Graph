82e5b2fffb3db0b063310c22cee82241f15707db
Andy SHU <andy091045@gmail.com>
2022-04-12 21:28:23 +0800

    write hw1~5 summry

--- cpp/Lab05.cpp
+++ cpp/Lab05.cpp
@@ -1,5 +1,5 @@
 /***C/C++  http://bbs.csdn.net/topics/100091584***/
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <Cmath>
@@ -7,155 +7,169 @@
 /***freeglut***/
 #include ".\GL\freeglut.h"
 
-//µe•¨™¯ºe
+//ÔøΩeÔøΩÔøΩÔøΩÔøΩÔøΩe
 #define canvasX 600
 #define canvasY 600
 
-//glOrtho Ωd≥Ú
+// glOrtho ÔøΩdÔøΩÔøΩ
 //-30~30
 #define regionX 60
 #define regionY 60
 
+//‰ΩúÊ•≠‰∫î: ÈªûÈÅ∏Á∂≤Ê†ºÁî¢ÁîüÊñπÂ°ä‰∏¶‰∏îÈ°ØÁ§∫Ë©≤ÊñπÂ°äÂ∫ßÊ®ô
 
 bool mouseClick = false;
 int colrow = 10;
 float px = 0, py = 0;
 
 void ChangeSize(int, int);
-void RenderScene(void);		
+void RenderScene(void);
 void buildPopupMenu();
 void changeGrid(int);
-void unit(int,int);
+void unit(int, int);
 void mymouse(GLint, GLint, GLint, GLint);
 
-int main(int argc, char** argv) 
-{ 
-   glutInit(&argc, argv); 
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  
-   glutInitWindowSize(canvasX, canvasY);
-   glutInitWindowPosition(600,80);            
-   glutCreateWindow("Simple Rectangle");        
-    
-   buildPopupMenu();  
-   glutMouseFunc(mymouse);
-   glutReshapeFunc(ChangeSize); 
-   glutDisplayFunc(RenderScene); 
-   glutMainLoop();	
-   return 0; 
-} 
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(canvasX, canvasY);
+    glutInitWindowPosition(600, 80);
+    glutCreateWindow("Simple Rectangle");
+
+    buildPopupMenu();
+    glutMouseFunc(mymouse);
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    glutMainLoop();
+    return 0;
+}
 
 void menuSelect(int option)
 {
-	switch(option)
-	{
-	case 0:
-		colrow = 10;
-		glutPostRedisplay();
-		break;
-	case 1:
-		colrow = 15;
-		glutPostRedisplay();
-		break;
-	case 2:
-		colrow = 20;
-		glutPostRedisplay();
-		break;
-	default:
-		break;
-	}
+    switch (option)
+    {
+    case 0:
+        colrow = 10;
+        glutPostRedisplay();
+        break;
+    case 1:
+        colrow = 15;
+        glutPostRedisplay();
+        break;
+    case 2:
+        colrow = 20;
+        glutPostRedisplay();
+        break;
+    default:
+        break;
+    }
 }
 
 void buildPopupMenu()
 {
-	int menu_id;
-	menu_id = glutCreateMenu(menuSelect);
-    glutAddMenuEntry("10",0);
-	glutAddMenuEntry("15",1);
-    glutAddMenuEntry("20",2);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-
+    int menu_id;
+    menu_id = glutCreateMenu(menuSelect);
+    glutAddMenuEntry("10", 0);
+    glutAddMenuEntry("15", 1);
+    glutAddMenuEntry("20", 2);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
-void ChangeSize(int w, int h) 
-{ 
-   printf("Window Size= %d X %d\n",w,h); 
-   glViewport(0, 0, w, h);             
-   glMatrixMode(GL_PROJECTION); 
-   glLoadIdentity(); 
-   glOrtho(-(regionX/2), regionX / 2, -(regionY / 2), regionY / 2,-30,30);
-   glMatrixMode(GL_MODELVIEW); 
-   glLoadIdentity(); 
-} 
-void RenderScene(void) 
-{ 
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, 0, 1.0f, 0, 0, 0, 0, 1, 0);
-	
-	//draw Grid 
-	changeGrid(colrow);
+void ChangeSize(int w, int h)
+{
+    printf("Window Size= %d X %d\n", w, h);
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-(regionX / 2), regionX / 2, -(regionY / 2), regionY / 2, -30, 30);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+void RenderScene(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0, 1.0f, 0, 0, 0, 0, 1, 0);
 
-	//draw Square Unit
-	if (mouseClick) {
-		//if Exceed the range of colrow don't draw the unit
-		if(px<=colrow && px>=-colrow){
-			if (py <= colrow && py >= -colrow) {
-				//draw unit
-				glBegin(GL_TRIANGLES);
-				glColor3f(1, 0, 0); glVertex3f(px, py + 1, 0);
-				glColor3f(1, 0, 0); glVertex3f(px + 1, py, 0);
-				glColor3f(1, 0, 0); glVertex3f(px + 1, py + 1, 0);
+    // draw Grid
+    changeGrid(colrow);
 
+    // draw Square Unit
+    if (mouseClick)
+    {
+        // if Exceed the range of colrow don't draw the unit
+        if (px <= colrow && px >= -colrow)
+        {
+            if (py <= colrow && py >= -colrow)
+            {
+                // draw unit
+                glBegin(GL_TRIANGLES);
+                glColor3f(1, 0, 0);
+                glVertex3f(px, py + 1, 0);
+                glColor3f(1, 0, 0);
+                glVertex3f(px + 1, py, 0);
+                glColor3f(1, 0, 0);
+                glVertex3f(px + 1, py + 1, 0);
 
-				glColor3f(1, 0, 0); glVertex3f(px, py + 1, 0);
-				glColor3f(1, 0, 0); glVertex3f(px, py, 0);
-				glColor3f(1, 0, 0); glVertex3f(px + 1, py, 0);
+                glColor3f(1, 0, 0);
+                glVertex3f(px, py + 1, 0);
+                glColor3f(1, 0, 0);
+                glVertex3f(px, py, 0);
+                glColor3f(1, 0, 0);
+                glVertex3f(px + 1, py, 0);
 
-				glEnd();
-			}
-		}				
-	}
+                glEnd();
+            }
+        }
+    }
 
-    glutSwapBuffers(); 
+    glutSwapBuffers();
 }
 
-//draw Grid
-void changeGrid(int a) {
-	for (int x = -a; x <= a; x++) {
-		for (int y = -a; y <= a; y++) {
-			unit(x, y);
-		}		
-	}
+// draw Grid
+void changeGrid(int a)
+{
+    for (int x = -a; x <= a; x++)
+    {
+        for (int y = -a; y <= a; y++)
+        {
+            unit(x, y);
+        }
+    }
 }
 
-void unit(int x, int y) {
-	glLineWidth(1.0);
-	glColor3f(1.0, 0.0, 0.0);
+void unit(int x, int y)
+{
+    glLineWidth(1.0);
+    glColor3f(1.0, 0.0, 0.0);
 
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(x, y);
-	glVertex2f(x+1, y);
-	glVertex2f(x+1, y+1);
-	glVertex2f(x, y+1);
-	glEnd();
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(x, y);
+    glVertex2f(x + 1, y);
+    glVertex2f(x + 1, y + 1);
+    glVertex2f(x, y + 1);
+    glEnd();
 }
 
-void mymouse(GLint button, GLint state, GLint x, GLint y) {
+void mymouse(GLint button, GLint state, GLint x, GLint y)
+{
 
-	switch (button)
-	{
-	case GLUT_LEFT_BUTTON:
-		if (state == GLUT_UP) {
-			px = ceil(x / (canvasX / regionX)-30);
-			py = ceil((canvasY - y) / (canvasY / regionY)-30) ;
-			std::cout << "xÆyº–: " << px << "\n";
-			std::cout << "yÆyº–: " << py << "\n"<< "\n";
-			mouseClick = true;
-		}
-	default:
-		break;
-	}
-
+    switch (button)
+    {
+    case GLUT_LEFT_BUTTON:
+        if (state == GLUT_UP)
+        {
+            px = ceil(x / (canvasX / regionX) - 30);
+            py = ceil((canvasY - y) / (canvasY / regionY) - 30);
+            std::cout << "xÔøΩyÔøΩÔøΩ: " << px << "\n";
+            std::cout << "yÔøΩyÔøΩÔøΩ: " << py << "\n"
+                      << "\n";
+            mouseClick = true;
+        }
+    default:
+        break;
+    }
 }
--- cpp/Rotate_cube.cpp
+++ cpp/Rotate_cube.cpp
@@ -1,163 +1,169 @@
 /***C/C++  http://bbs.csdn.net/topics/100091584***/
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 /***freeglut***/
 #include ".\GL\freeglut.h"
- 
-void ChangeSize(int , int );					 
-void RenderScene(void);		
+
+//作業二:旋轉位移方塊(可以用glut API)
+
+void ChangeSize(int, int);
+void RenderScene(void);
 void SetupRC();
 void myKeyboard(unsigned char, int, int);
 void mySpecialKey(int, int, int);
 int tx = 0, ty = 0, tz = 0;
 int thetaX = 0, thetaY = 0, thetaZ = 0;
 
-int main(int argc, char** argv) 
-{ 
-   glutInit(&argc, argv); 
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  
-   glutInitWindowSize(400,400);				 
-   glutInitWindowPosition(600,80);            
-   glutCreateWindow("Moving Cube"); 
-   SetupRC();    
-     
-   glutReshapeFunc(ChangeSize); 
-   glutDisplayFunc(RenderScene); 
-   glutKeyboardFunc(myKeyboard);
-   glutSpecialFunc(mySpecialKey);
-   glutMainLoop();	
-   return 0; 
-} 
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(600, 80);
+    glutCreateWindow("Moving Cube");
+    SetupRC();
 
-void SetupRC(){
-	GLfloat whiteLight[] = { 0.45f,0.45f,0.45f,1.0f };
-	GLfloat sourceLight[] = { 0.25f,0.25f,0.25f,1.0f };
-	GLfloat lightPos[] = { 0.f,25.0f,20.0f,0.0f };
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    glutKeyboardFunc(myKeyboard);
+    glutSpecialFunc(mySpecialKey);
+    glutMainLoop();
+    return 0;
+}
 
-	//Enable lighting
-	glEnable(GL_LIGHTING);
+void SetupRC()
+{
+    GLfloat whiteLight[] = {0.45f, 0.45f, 0.45f, 1.0f};
+    GLfloat sourceLight[] = {0.25f, 0.25f, 0.25f, 1.0f};
+    GLfloat lightPos[] = {0.f, 25.0f, 20.0f, 0.0f};
 
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT,whiteLight);
-	glLightfv(GL_LIGHT0,GL_AMBIENT,sourceLight);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, sourceLight);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-	glEnable(GL_LIGHT0);
+    // Enable lighting
+    glEnable(GL_LIGHTING);
 
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_DEPTH_TEST);
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, sourceLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, sourceLight);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+    glEnable(GL_LIGHT0);
+
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_DEPTH_TEST);
 }
 
 void menuSelect(int option)
 {
-	switch(option)
-	{
-	case 0:
-		glShadeModel(GL_FLAT);
-		glutPostRedisplay();
-		break;
-	case 1:
-		glShadeModel(GL_SMOOTH);
-		glutPostRedisplay();
-		break;
-	default:
-		break;
-	}
+    switch (option)
+    {
+    case 0:
+        glShadeModel(GL_FLAT);
+        glutPostRedisplay();
+        break;
+    case 1:
+        glShadeModel(GL_SMOOTH);
+        glutPostRedisplay();
+        break;
+    default:
+        break;
+    }
 }
 
 void buildPopupMenu()
 {
-	int menu_id;
-	menu_id = glutCreateMenu(menuSelect);
-    glutAddMenuEntry("Flat",0);
-    glutAddMenuEntry("Smooth",1);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-
+    int menu_id;
+    menu_id = glutCreateMenu(menuSelect);
+    glutAddMenuEntry("Flat", 0);
+    glutAddMenuEntry("Smooth", 1);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
-void ChangeSize(int w, int h) 
-{ 
-   glViewport(0, 0, w, h);             
-   glMatrixMode(GL_PROJECTION); 
-   glLoadIdentity(); 
-   glOrtho(-10,10,-10,10,-10,20);       
-   glMatrixMode(GL_MODELVIEW); 
-   glLoadIdentity(); 
-} 
-void RenderScene(void) 
-{ 
-   glClearColor(1.0, 1.0, 1.0, 1.0);  
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-   glMatrixMode(GL_MODELVIEW); 
-   glLoadIdentity(); 
-   gluLookAt(10.0f,10.0f,10.0f,0,0,0,0,1,0);
-   
-   //draw X-axis, Y-axis , Z-axis
-   glColor3f(1.0f, 0.0f, 0.0f);
-   glBegin(GL_LINES);
-   glVertex3f(-100, 0, 0);
-   glVertex3f(100, 0, 0);
-   glVertex3f(0, -100, 0);
-   glVertex3f(0, 100, 0);
-   glVertex3f(0, 0, -100);
-   glVertex3f(0, 0, 100);
-   glEnd();
+void ChangeSize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-10, 10, -10, 10, -10, 20);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+void RenderScene(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(10.0f, 10.0f, 10.0f, 0, 0, 0, 0, 1, 0);
 
-   //cube
-   glRotatef(thetaX, 1,0,0);
-   glRotatef(thetaY, 0, 1, 0);
-   glRotatef(thetaZ, 0, 0, 1);
-   glTranslatef(tx, ty, tz);
-   glColor3f(1, 1, 0);
-   glutSolidCube(6);
-   glutSwapBuffers(); 
+    // draw X-axis, Y-axis , Z-axis
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_LINES);
+    glVertex3f(-100, 0, 0);
+    glVertex3f(100, 0, 0);
+    glVertex3f(0, -100, 0);
+    glVertex3f(0, 100, 0);
+    glVertex3f(0, 0, -100);
+    glVertex3f(0, 0, 100);
+    glEnd();
+
+    // cube
+    glRotatef(thetaX, 1, 0, 0);
+    glRotatef(thetaY, 0, 1, 0);
+    glRotatef(thetaZ, 0, 0, 1);
+    glTranslatef(tx, ty, tz);
+    glColor3f(1, 1, 0);
+    glutSolidCube(6);
+    glutSwapBuffers();
 }
 
-void myKeyboard(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'r':
-		thetaX = 0;
-		thetaY = 0;
-		thetaZ = 0;	
-		tx = 0;
-		ty = 0;
-		tz = 0;
-		break;
-	case 'a':		
-		thetaX += 3;		
-		break;
-	case 'd':
-		thetaX -= 3;		
-		break;
-	case 'w':
-		thetaY += 3;		
-		break;
-	case 's':
-		thetaY -= 3;		
-		break;
-	case 'z':
-		thetaZ += 3;		
-		break;
-	case 'x':
-		thetaZ -= 3;		
-		break;
-	}
-	glutPostRedisplay();
+void myKeyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'r':
+        thetaX = 0;
+        thetaY = 0;
+        thetaZ = 0;
+        tx = 0;
+        ty = 0;
+        tz = 0;
+        break;
+    case 'a':
+        thetaX += 3;
+        break;
+    case 'd':
+        thetaX -= 3;
+        break;
+    case 'w':
+        thetaY += 3;
+        break;
+    case 's':
+        thetaY -= 3;
+        break;
+    case 'z':
+        thetaZ += 3;
+        break;
+    case 'x':
+        thetaZ -= 3;
+        break;
+    }
+    glutPostRedisplay();
 }
 
-void mySpecialKey(int key, int x, int y) {
-	switch (key) {
-	case GLUT_KEY_LEFT:
-		tx -= 2;
-		break;
-	case GLUT_KEY_RIGHT:
-		tx += 2;
-		break;
-	case GLUT_KEY_UP:
-		ty -= 2;
-		break;
-	case GLUT_KEY_DOWN:
-		ty += 2;
-		break;
-	}
-	glutPostRedisplay();
+void mySpecialKey(int key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_LEFT:
+        tx -= 2;
+        break;
+    case GLUT_KEY_RIGHT:
+        tx += 2;
+        break;
+    case GLUT_KEY_UP:
+        ty -= 2;
+        break;
+    case GLUT_KEY_DOWN:
+        ty += 2;
+        break;
+    }
+    glutPostRedisplay();
 }
